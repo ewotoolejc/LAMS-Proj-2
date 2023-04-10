79b6be60df3d5803d60735cd0654990ee7c2d831
ewotoolejc <ewotoolejc@gmail.com>
2023-04-10 17:33:26 -0400

    Day 4 EOB, more styling and implementation of full logged in capabilities

--- routes/artists.js
+++ routes/artists.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var artistsCtrl = require('../controllers/artists');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 router.get("/artists", artistsCtrl.index);
-router.get("/artists/new", artistsCtrl.new);
+router.get("/artists/new", ensureLoggedIn, artistsCtrl.new);
 router.get("/artists/:id", artistsCtrl.details);
-router.get('/artists/:id/edit', artistsCtrl.edit)
-router.post("/artists", artistsCtrl.create);
+router.get('/artists/:id/edit', ensureLoggedIn, artistsCtrl.edit)
+router.post("/artists", ensureLoggedIn, artistsCtrl.create);
 
-router.delete('/artists/:id', artistsCtrl.delete);
+router.delete('/artists/:id', ensureLoggedIn, artistsCtrl.delete);
 
-router.put('/artists/:id', artistsCtrl.update);
-router.put('/deals/:id/artists', artistsCtrl.addArtisttoDeal);
-router.put('/artists/:id/signed', artistsCtrl.updateToSigned);
+router.put('/artists/:id', ensureLoggedIn, artistsCtrl.update);
+router.put('/deals/:id/artists', ensureLoggedIn, artistsCtrl.addArtisttoDeal);
+router.put('/artists/:id/signed', ensureLoggedIn, artistsCtrl.updateToSigned);
 
 module.exports = router;
